ecdfc49859412151db745ca8a7f88d9337cde101
Casey LP <11209477+soup-bowl@users.noreply.github.com>
2026-07-20 12:51:22 +0000

    Auto-checkout

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -2,7 +2,21 @@
 
 pre-commit install
 
+# Clone companion repositories if not already present (no-op in Codespaces where they are cloned automatically)
+repos=(
+	"soup-bowl/servers"
+	"soup-bowl/tf-practice"
+)
+
+for repo in "${repos[@]}"; do
+	name="${repo#*/}"
+	if [ ! -d "/workspaces/${name}" ]; then
+		gh repo clone "${repo}" "/workspaces/${name}" || git clone "https://github.com/${repo}.git" "/workspaces/${name}"
+	fi
+done
+
 # Azure
+
 az extension add --name bastion
 az extension add --name ssh
 
